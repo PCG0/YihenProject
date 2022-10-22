65fb92733da72821217cd7f5fde0e42bb42dbda9
Yu1 <2553016560@qq.com>
2022-10-22 20:11:57 +0800

    人物跟随鼠标转向

---
+++ Assets/Scripts/Player/AutoTurn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoTurn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < Camera.main.ScreenToWorldPoint(Input.mousePosition).x) 
+            // transform.eulerAngles = new Vector3(0, 0, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        if (transform.position.x > Camera.main.ScreenToWorldPoint(Input.mousePosition).x)
+            // transform.localScale = new Vector3(-1, 1, 1);
+            transform.eulerAngles = new Vector3(0, 180, 0);
+    }
+}
